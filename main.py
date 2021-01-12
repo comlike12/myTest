1ac34c16a1fb6565560b33d6a50e86d6f0b08e6e
comlike12 <comlike12@kakao.com>
2021-01-12 17:02:50 +0900

    test update 14

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def print_hi(name):
 # 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print_hi('짜증난다..................')
 
 
 # https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 확인
